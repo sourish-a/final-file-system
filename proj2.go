621c5ba1aab3fb9479663e957455248449929079
Sourish Agarwal <sourish@berkeley.edu>
2021-04-14 02:23:05 -0700

    pseudocode complete

--- proj2.go
+++ proj2.go
@@ -153,6 +153,31 @@ func (userdata *User) StoreFile(filename string, data []byte) (err error) {
 	userlib.DatastoreSet(storageKey, jsonData)
 	//End of toy implementation
 
+	// Load the namespace map from the datastore using UUID
+	// If the filename exists in the namespace:
+	// 		Load the FileFrame Struct
+	//		If I'm the owner, load the key and FileUUID from the FileFrameStruct directly
+	// 		Otherwise load the SharedFileFrame from the DataStore and get the key and FileUUID from there
+	// 		Load FileStruct from Datastore using FileUUID, unencrypt and ensure validity
+	//		Change appends to 1
+	//		Create new AppendNode and set the data to filedata and set next to null
+	// 		create a new UUID and set firstAppend and lastAppend to this new UUID
+	// 		encrypt the appendNode with HKDF(key, 1) and store it in the datastore at the new UUID
+	//		encrypt and MAC the FileStruct and resave at same UUID
+	//		
+	// If the filename does not exist in the namespace:
+	// 		Create a FileFrame Struct for that hashed filename, 
+	//		Set the owner value to true, generate UUID for file, generate encryp/decryp key (HKDF(owners master, 16 random bytes)), create map of shared users to access tokens
+	// 		Create a new FileStruct, set appends to 1
+	//		Generate an AppendNodeUUID and set firstAppend and lastAppend to that UUID
+	//		Create an AppendNode, set the filedata to data, set nextNode to Null
+	//		Encrypt + Mac and save AppendNode at the new AppendNodeUUID
+	// 		Update the owner of the new file to myself
+	//		Encrypt + Mac and save FileStruct at new FileStructUUID
+
+	hashedFilename := Hash(filename)
+	userdata.
+
 	return
 }
 
@@ -160,6 +185,26 @@ func (userdata *User) StoreFile(filename string, data []byte) (err error) {
 // https://cs161.org/assets/projects/2/docs/client_api/appendfile.html
 func (userdata *User) AppendFile(filename string, data []byte) (err error) {
 	return
+
+	// from the namespace map, use the hash(filename) as the key to get the FileFrameStruct
+	// if NOT the owner:
+	// 		load the SharedFileFrame from the datastore using the SharedFrame UUID in the FileFrameStruct
+	// 		get encryp/decryption key and FileUUID from the SharedFileFrame struct
+	// if owner:
+	//		load the key and FileUUID from the FileFrameStruct directly
+	// load the FileStruct from the DataStore using the FileUUID
+	// unencrypt the FileStruct using the key and check HMAC for validity
+	// if HMAC is invalid -- Error: file has been tampered with
+	// else if data cannot be unmarshalled -- Error: user no longer has access, remove filename from namespace
+	// Generate a new random UUID called newNode
+	// Load AppendNode from LastAppendUUID (from the Datastore) and unencrypt it by HKDF(Symmkey, appends) and check MAC
+	// Set nextAppendNode to this new UUID
+	// Reencrypt and MAC using HKDF(symmkey, appends), put back in datastore at LastAppendUUID
+	// Change LastAppendUUID to newNode
+	// increment appends by 1 in the FileStruct
+	// Create new AppendNodeStruct, and Save the new filedata to the AppendNode, and set the next node to null
+	// Encrypt + MAC the append node using HKDF(Symmkey, appends), save in Datastore with the newly generated UUID
+	
 }
 
 // LoadFile is documented at:
@@ -176,6 +221,15 @@ func (userdata *User) LoadFile(filename string) (dataBytes []byte, err error) {
 	return dataBytes, nil
 	//End of toy implementation
 
+	// If namespace does not contain hash(filename), return error
+	// Load FileFrameStruct UUID
+	// If file owner, load fileStructUUID and key directly
+	// Otherwise load and decrypt SharedFileFrame, and from there load fileStructUUID and decryption key
+	// Load and decrypt fileStruct and verify validity (invalid then error)
+	// Until null, iterate through appendNodes:
+	//		For each appendNode, load in the appendNode and decrypt and verify validity (invalid then error)
+	// 		load in the data and append to dataBytes
+	// return the dataBytes and an error if any
 	return
 }
 
@@ -183,7 +237,21 @@ func (userdata *User) LoadFile(filename string) (dataBytes []byte, err error) {
 // https://cs161.org/assets/projects/2/docs/client_api/sharefile.html
 func (userdata *User) ShareFile(filename string, recipient string) (
 	accessToken uuid.UUID, err error) {
-
+	// Check that the file is in the namespace or error
+	// Grab the FileFrameStruct
+	// If user is the owner of the file, 
+	//		create a new SharedFileFrame
+	// 		Save the encryption key
+	// 		Save the FileUUID
+	// 		Generate a new AccessCode and encrypt + MAC the SharedFileFrame
+	//		Save the SharedFileFrame in the datastore at a newUUID
+	//		Update the user/accesstoken map to include the recipient and their accesstoken
+	// Create a new SharedFileInvitationStruct
+	// Save the UUID of the SharedFileFrame
+	// Save the accesstoken of the recipient
+	// Encrypt with recipient's public RSA key from the keystore
+	// Store the SharedFileInvitationStruct at a UUID generated from the hash(filename + recipient)
+	// return where this is stored
 	return
 }
 
@@ -191,11 +259,29 @@ func (userdata *User) ShareFile(filename string, recipient string) (
 // https://cs161.org/assets/projects/2/docs/client_api/receivefile.html
 func (userdata *User) ReceiveFile(filename string, sender string,
 	accessToken uuid.UUID) error {
+	// TODO: Need to verify integrity of SharedFileInvitation as well, can something be MACd and signed?
+	// if namespace.contains(filename), Error: You already contain a file with the same name.
+	// Check that a struct exists at parameter accessToken
+	// Load in the struct and decrypt it using RSA private key and verify DS
+	// If cannot verify DS, Error: Sender cannot be verified
+	// Load the accessToken and decrypt the IntermediateStruct
+	// If revoked == true, Error: revoked
+	// Save IntermediateStruct UUID in FileFrameStruct
 	return nil
 }
 
 // RevokeFile is documented at:
 // https://cs161.org/assets/projects/2/docs/client_api/revokefile.html
 func (userdata *User) RevokeFile(filename string, targetUsername string) (err error) {
+	// if !namespace.containsKey(filename), Error: No such file
+	// if !user/accesstoken_map(targetUsername), Error: File not shared with specified user
+	// Retrieve FileFrameStruct from namespace
+	// Retrieve UUID of IntermediateStruct + Accesskey to decrypt from user/uuid table
+	// Set revoked boolean to true, set file UUID to 0, set encryption decryption key to 0
+	// Iterate through keyset of shared users (except targetUsername) and change encryption/decryption keys
+	// Load FileStruct, decrypt w/ old key, Load FirstAppend, decrypt w/ old key
+	// till null, reencrypt w new key and put back
+	// reencrypt fileStruct
+	// remove user from FileFrameStruct.user/uuid_map
 	return
 }
